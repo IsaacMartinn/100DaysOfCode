32c254d214fa64d700865fa8c80ec3220324e895
Isaac Martin <143645612+IsaacMartinn@users.noreply.github.com>
2024-11-03 13:27:29 -0500

    Update main.py

--- Day 050 (Auto Tinder Bot)/main.py
+++ Day 050 (Auto Tinder Bot)/main.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-PHONE_NUM = "4168852667"
-PASS = "Isaacmax2002"
+PHONE_NUM = "xxxxxx"
+PASS = "xxxxxxxx"
 
 URL = "https://www.tinder.com"
 chrome_options = webdriver.ChromeOptions()
