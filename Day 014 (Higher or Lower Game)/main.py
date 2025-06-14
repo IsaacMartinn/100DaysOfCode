2cb150bb71abead9b0eb3655148bb90ef00fff21
Isaac Martin <isaacisamartin@gmail.com>
2025-06-14 15:16:13 -0400

    Day 14 incomplete

---
+++ Day 014 (Higher or Lower Game)/main.py
@@ -0,0 +1,36 @@
+from art import logo, vs
+from game_data import data
+import random 
+
+# print(logo)
+# print(len(data))
+# print(data[49])
+
+score = 0 
+game_on = True
+
+
+while game_on:
+    comparison_a = data[random.randint(0,len(data)-1)]
+    comparison_b = data[random.randint(0,len(data)-1)]
+    print(comparison_a)
+    print(comparison_b)
+
+    print(logo)
+    print(f"Compare A: {comparison_a['name']}, {comparison_a['description']}, from {comparison_a['country']}")
+    print(vs)
+    print(f"Against B: {comparison_b['name']}, {comparison_b['description']}, from {comparison_b['country']}")
+    user_choice = input(f"Who has more followers? Type 'A' or 'B': ").lower()
+    # if statement judging follower count
+    if user_choice == "a" and comparison_a['follower_count'] > comparison_b['follower_count']:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    elif user_choice == "b" and comparison_b['follower_count'] > comparison_a["follower_count"]:
+        score += 1
+        print(f"You're right! Current scorelol: {score}")
+    else:
+        print("Invalid option")
+
+
+
+
