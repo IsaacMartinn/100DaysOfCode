2d5042e6e4f201da760701cca551696b7b76d5c4
Isaac Martin <isaacisamartin@gmail.com>
2025-07-01 15:06:41 -0400

    Day 22

--- Day 022 (Pong)/main.py
+++ Day 022 (Pong)/main.py
@@ -1,5 +1,9 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time 
+from scoreboard import Scoreboard
+
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -16,9 +20,36 @@ screen.onkey(fun=r_paddle.down,key="Down")
 screen.onkey(fun=l_paddle.up,key="w")
 screen.onkey(fun=l_paddle.down,key="s")
 
+ball = Ball()
+scoreboard = Scoreboard()
+
 game_is_on = True
 while game_is_on: 
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    #Detect collision with the ball 
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) <50 and ball.xcor() <-320:
+        ball.bounce_x()
+        
+    
+    #Detect R paddle misses
+    if ball.xcor() > 380: 
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #Detect if L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
 
 
 screen.exitonclick()
