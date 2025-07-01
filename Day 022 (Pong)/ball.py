c09bb9311a17d389e4e0ff8aadbdea1b668ced32
Isaac Martin <isaacisamartin@gmail.com>
2025-07-01 12:58:08 -0400

    Day 22 incomplete

---
+++ Day 022 (Pong)/ball.py
@@ -0,0 +1,10 @@
+from turtle import Turtle
+import random
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        #width = 20
+        #height = 20
---
+++ Day 022 (Pong)/main.py
@@ -0,0 +1,24 @@
+from turtle import Screen
+from paddle import Paddle
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+
+screen.listen()
+screen.onkey(fun=r_paddle.up,key="Up")
+screen.onkey(fun=r_paddle.down,key="Down")
+screen.onkey(fun=l_paddle.up,key="w")
+screen.onkey(fun=l_paddle.down,key="s")
+
+game_is_on = True
+while game_is_on: 
+    screen.update()
+
+
+screen.exitonclick()
